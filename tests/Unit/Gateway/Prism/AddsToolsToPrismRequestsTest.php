7b065805e97f16ef6d80d19792f52f44b60930d0
Anilcan Cakir <anilcan.cakir@gmail.com>
2026-02-12 15:54:10 +0300

    revert: restore AddsToolsToPrismRequestsTest to 0.x original

--- tests/Unit/Gateway/Prism/AddsToolsToPrismRequestsTest.php
+++ tests/Unit/Gateway/Prism/AddsToolsToPrismRequestsTest.php
@@ -147,7 +147,7 @@ class AddsToolsToPrismRequestsTest extends TestCase
                 $this->toolInvokedCallback = fn () => null;
             }
 
-            public function test_invoke_tool(Tool $tool, array $arguments): string
+            public function testInvokeTool(Tool $tool, array $arguments): string
             {
                 return $this->invokeTool($tool, $arguments);
             }
